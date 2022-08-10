917883e866205b8c5f05cac9c31681d5e1cbdf84
Dev <devextraemail@gmail.com>
2022-08-09 23:45:48 -0400

    added game timers and game over screen-- done w basic functionality

--- index.js
+++ index.js
@@ -109,12 +109,41 @@ const keys = {
     }
 }
 
+function determineWinner({player,enemy, timerId}){
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if(player.health === enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    } else if (player.health > enemy.health){
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+    } else if (enemy.health > player.health){
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+    }
+}
+
 function rectangularCollision({rectangle1, rectangle2}){
     return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x 
         && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
         && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
         && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height)
 }
+
+let timer = 60
+let timerId
+function decreaseTimer(){
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+    if (timer === 0) {
+        document.querySelector('#displayText').style.display = 'flex'
+        determineWinner({player, enemy})
+    }
+}
+
+decreaseTimer()
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -156,6 +185,11 @@ function animate(){
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0){
+        determineWinner({player,enemy,timerId})
     }
 }
 
