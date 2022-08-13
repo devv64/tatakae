b2e688ba9896b7c60978c2b2d265d35e95264de3
Dev <devextraemail@gmail.com>
2022-08-12 23:17:29 -0400

    added shop w animation

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ const background = new Sprite({
     imageSrc: './img/background.png'
 })
 
+const shop = new Sprite({
+    position: {
+        x:600,
+        y:128
+    },
+    imageSrc: './img/shop.png',
+    scale: 2.75,
+    framesMax: 6
+})
+
 const player = new Fighter({
     position: {
     x: 0,
@@ -64,40 +74,6 @@ const keys = {
     }
 }
 
-function determineWinner({player,enemy, timerId}){
-    clearTimeout(timerId)
-    document.querySelector('#displayText').style.display = 'flex'
-    if(player.health === enemy.health){
-        document.querySelector('#displayText').innerHTML = 'Tie'
-    } else if (player.health > enemy.health){
-        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
-    } else if (enemy.health > player.health){
-        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
-    }
-}
-
-function rectangularCollision({rectangle1, rectangle2}){
-    return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x 
-        && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
-        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
-        && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height)
-}
-
-let timer = 60
-let timerId
-function decreaseTimer(){
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000)
-        timer--
-        document.querySelector('#timer').innerHTML = timer
-    }
-
-    if (timer === 0) {
-        document.querySelector('#displayText').style.display = 'flex'
-        determineWinner({player, enemy})
-    }
-}
-
 decreaseTimer()
 
 function animate(){
@@ -105,6 +81,7 @@ function animate(){
     c.fillStyle = 'black'
     c.fillRect(0,0,canvas.width,canvas.height)
     background.update()
+    shop.update()
     player.update()
     enemy.update()
 
